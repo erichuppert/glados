d1ad4c0e00e8649d327c968cb34cf46e4ad5d020
Eric Huppert <eric@erichuppert.com>
2015-03-02 16:21:42 -0500

    change LocationNavigation initial state

--- lab5/src/LocalNavigation/LocalNavigation.java
+++ lab5/src/LocalNavigation/LocalNavigation.java
@@ -31,7 +31,7 @@ public class LocalNavigation implements NodeMain,Runnable{
 	public static final int ALIGN_ON_BUMP        =  1;
 	public static final int ALIGNING             =  2;
 	public static final int ALIGNED              =  3;
-	private int state = STOP_ON_BUMP;
+	private int state = ALIGN_ON_BUMP;
 	
 	public Subscriber<org.ros.message.rss_msgs.SonarMsg> sonarFrontSub, sonarBackSub;
 	public Subscriber<org.ros.message.rss_msgs.BumpMsg> bumpSub;
