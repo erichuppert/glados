dcdab9d72b0ab214f2a98c6450aea382c5945f6a
Rodrigo Toste Gomes <rgomes@mit.edu>
2015-03-04 02:50:42 -0500

    Highly experimental code refactoring - probably not working out of the box

---
+++ lab5/src/LocalNavigation/FSM.java
@@ -0,0 +1,179 @@
+package LocalNavigation;
+
+import org.ros.node.Node;
+import org.ros.node.topic.Publisher;
+import org.ros.message.std_msgs.String;
+import org.ros.message.rss_msgs.MotionMsg;
+
+public class FSM {
+	// Frequency that step should be called
+	//
+	public static final double FREQ = 20;
+
+	// Possible States
+	//
+	public static final java.lang.String STOP_ON_BUMP      = "Initial state: stops when it feels a bump";
+	public static final java.lang.String ALIGN_ON_BUMP     = "Initial state: aligns when it feels a bump";
+	public static final java.lang.String ALIGNING          = "Currently aligining the robot";
+	public static final java.lang.String ALIGNED           = "Currently aligned";
+
+	// State variable
+	//
+	private java.lang.String state;
+
+	// Inputs
+	//
+	private double[] sonars;
+	private double[] pose;
+	private boolean[] bumpers;
+
+	// ROS publishers
+	//
+	private Publisher<org.ros.message.std_msgs.String> statePub; // state
+	private Publisher<MotionMsg> motorPub; // motor commands
+
+	// Set the Initial State
+	//
+	public FSM(Node node, java.lang.String initialState) {
+		statePub = node.newPublisher("/rss/state","std_msgs/String");
+		motorPub = node.newPublisher("/command/Motors","rss_msgs/MotionMsg");
+		changeState(initialState);
+	}
+
+	// Translational/Rotational velocities
+	// So we don't have to redeclare them in every method.
+	// Set by methods to change motor velocities.
+	//
+	private boolean setVelocities;
+	private double tv,rv;
+
+	/**
+	 * Take a step in the state machine
+	 * @param _sonars:  most recent sonar values
+	 * @param _pose:    most recent odometry pose
+	 * @param _bumpers: most recent bumper statuses
+	 */
+	public void step(double[] _sonars, double[] _pose, boolean[] _bumpers) {
+		// Make inputs available to all methods
+		//
+		sonars = _sonars;
+		pose = _pose;
+		bumpers = _bumpers;
+
+		// By default, we don't set velocities
+		//
+		setVelocities = false;
+
+		// Execute the method for the current state
+		//
+		switch(state) {
+		case STOP_ON_BUMP:
+			stop_on_bump();
+			break;
+		case ALIGN_ON_BUMP:
+			align_on_bump();
+			break;
+		case ALIGNING:
+			aligning();
+			break;
+		case ALIGNED:
+			aligned();
+			break;
+		default:
+			throw new Exception("INVALID STATE");
+		}
+
+		if (setVelocities) {
+			setMotorVelocities(tv,rv);
+		}
+	}
+
+	// Below are values that have been tuned based on experimentation
+	//
+	private static float ALIGNMENT_TRANSLATIONAL_SPEED = (float) 0.2;
+	private static float ALIGNMENT_ROTATIONAL_SPEED = (float) 0.05;
+
+	// Binary manipulations to make bumper testing simpler
+	//
+	private static final int bLEFT = 0b10;
+	private static final int bRIGHT = 0b01;
+	private static final int bNONE = 0b00;
+	private static int bumperStatus() {
+		return (bumpers[g.LEFT]?bLEFT:bNONE) | (bumpers[g.RIGHT]?bRIGHT:bNONE);
+	}
+
+	// If we see a bump, then stop, otherwise we are controlled externally
+	//
+	private void stop_on_bump() {
+		if (bumpers[g.LEFT] || bumpers[g.RIGHT]) {
+			tv = rv = 0;
+			setVelocities = true;
+		}
+	}
+
+	// If we see a bump, start aligning
+	//
+	private void align_on_bump() {
+		if (bumpers[g.LEFT] || bumpers[g.RIGHT]) {
+			changeState(ALIGNING);
+		}
+	}
+
+	// If we see a bump, then rotate to align the other.
+	// If both bumpers are pressed, then we're done aligning.
+	// Otherwise, look for wall by moving forward.
+	//
+	private void aligning() {
+		int bumperStatus = bumperStatus();
+		setVelocities = true;
+
+		switch(bumperStatus) {
+		case bRIGHT|bLEFT:
+			// Both bumpers pressed
+			//
+			changeState(ALIGNED);
+			tv = rv = 0;
+			break;
+		case bLEFT:
+		case bRIGHT:
+			// Either bumper pressed
+			// Rotate to align.
+			// Slight forward speed to avoid oscillations
+			// Happened due to depressed bumper becoming undepressed in pure rotation
+			//
+			tv = ALIGNMENT_TRANSLATIONAL_SPEED*0.1;
+			rv = ALIGNMENT_ROTATIONAL_SPEED * (bumpers[LEFT]?1.0:-1.0);
+		case bNONE:
+			// Neither is depressed, just move slowly forward.
+			//
+			tv = ALIGNMENT_TRANSLATIONAL_SPEED;
+			rv = 0;
+		}
+	}
+
+	// If we're aligned, we do nothing
+	//
+	private void aligned() {}
+
+	// Changes state variable, and publishes it.
+	//
+	private void changeState(java.lang.String newState){
+		state = newState;
+		org.ros.message.std_msgs.String msg = new org.ros.message.std_msgs.String();
+		msg.data = newState;
+		if(statePub != null) {
+			statePub.publish(msg);
+		}
+	}
+
+	// Useful for abstracting setting the motor velocities
+	//
+	private void setMotorVelocities(double tv, double rv) {
+		MotionMsg msg = new MotionMsg();
+		msg.translationalVelocity = tv;
+		msg.rotationalVelocity = rv;
+		if(motorPub != null) {
+			motorPub.publish(msg);
+		}
+	}
+}
--- lab5/src/LocalNavigation/LocalNavigation.java
+++ lab5/src/LocalNavigation/LocalNavigation.java
@@ -1,244 +1,104 @@
 package LocalNavigation;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.Scanner;
-import java.awt.Color;//for color from SonarGUI
-
 import org.ros.message.MessageListener;
-
-import org.ros.message.rss_msgs.*;
-import org.ros.message.std_msgs.*;
-import org.ros.message.lab5_msgs.*;
+import org.ros.message.rss_msgs.SonarMsg;
+import org.ros.message.rss_msgs.BumpMsg;
+import org.ros.message.rss_msgs.OdometryMsg;
 import org.ros.node.Node;
 import org.ros.node.NodeMain;
-import org.ros.node.topic.Publisher;
 import org.ros.node.topic.Subscriber;
 import org.ros.namespace.GraphName;
 
 public class LocalNavigation implements NodeMain,Runnable {
-	// State machine states, and state variable
+	// Kills run() on node shutdown
 	//
-	public static final java.lang.String STOP_ON_BUMP      = "Initial state: stops when it feels a bump";
-	public static final java.lang.String ALIGN_ON_BUMP     = "Initial state: aligns when it feels a bump";
-	public static final java.lang.String ALIGNING          = "Currently aligining the robot";
-	public static final java.lang.String ALIGNED           = "Currently aligned";
-	private java.lang.String state = "";
-
-	protected boolean firstUpdate = true;
-
-	/* Frames of reference:
-	 *
-	 * World frame: Relative to the robot's starting position, x front, y left, theta up
-	 * Robot frame: Relative to the robot right now, x front, y left, theta up
-	 * Sonar frame: Relative to the sonar, x points along the sonar axis, y left, theta up
-	 * Odometry frame: frame of reference the odometry module uses, the SonarGUI also uses the same.
-	 * Aligned frame: Relative to the robot when it most recently entered the ALIGNED state.
-	 */
-
-	// Transforms between robot and world frames
-	//
-	private Mat robotToWorld;
-
-	// Transforms between sonar and robot frames
-	//
-	private static final Mat sonarToRobotRot = Mat.rotation(Math.PI / 2);
-	private static final Mat sonarFrontToRobot = Mat.mul(Mat.translation(-0.064, 0.19), sonarToRobotRot);
-	private static final Mat sonarBackToRobot  = Mat.mul(Mat.translation(-0.3350, 0.2150), sonarToRobotRot);
+	private boolean shutdown = false;
 
 	// Subscribers
 	//
-	public Subscriber<SonarMsg> sonarFrontSub; // Sonars
-	public Subscriber<SonarMsg> sonarBackSub;
-	public Subscriber<BumpMsg> bumpSub; // Bump sensors
-	public Subscriber<OdometryMsg> odoSub; // Odometry
+	private Subscriber<SonarMsg>    sonarFrontSub; // Sonars
+	private Subscriber<SonarMsg>    sonarBackSub;
+	private Subscriber<BumpMsg>     bumpSub;       // Bump sensors
+	private Subscriber<OdometryMsg> odoSub;        // Odometry
 
-	// Message Publishers
+	// Helper objects
+	// They do the bulk of the work
 	//
-	public Publisher<MotionMsg> motorPub; // motors for velocity
-	public Publisher<org.ros.message.std_msgs.String> statePub; // Publish state value
-	public Publisher<GUILineMsg> linePub; // Publishes the Line to the GUI
-	//public Publisher<GUISegmentMsg> segmentPub; // Published Segment to the GUI; Not sure what it does
-	public Publisher<GUIPointMsg> pointPub; // Publishes points to the GUI
+	private SonarPoints sp;
+	private FSM fsm;
 
-	// Value decided on based on parameters of the problem.
+	// Global status
 	//
-	public double threshold_high = 0.6;
-	public double threshold_low = 0.1;
-
-	private LeastSquareLine lsqWorld;
-
-	// define colors for GUI
-	//
-	private static int RED=1, BLUE=2, GREEN=3;
-
-	// below are values that have been tuned based on experimentation
-	//
-	private static float ALIGNMENT_TRANSLATIONAL_SPEED = (float) 0.1;
-	private static float ALIGNMENT_ROTATIONAL_SPEED = (float) 0.05;
-
-	public LocalNavigation() {
-	}
+	double[] sonars  = new double[]         {0,0}; // {front,back}
+	double[] pose    = new double[]       {0,0,0}; // {x,y,theta}
+	double[] bumpers = new double[] {false,false}; // {left,right}
 
 	public void onStart(Node node) {
-		// initialize the ROS subscriptions to rss/Sonars
+		// Initialize helpers
 		//
-		sonarFrontSub = node.newSubscriber("/rss/Sonars/Front", "rss_msgs/SonarMsg");
-		sonarFrontSub.addMessageListener(new MessageListener<SonarMsg>() {
+		sp = new SonarPoints(node); // sonar point publisher/line maker
+		fsm = new FSM(node,FSM.ALIGN_ON_BUMP); // state machine
+
+		// Initialize subscriptions
+		//
+		sonarFrontSub = node.newSubscriber("/rss/Sonars/Front", "rss_msgs/SonarMsg"); // front sonar
+		sonarBackSub = node.newSubscriber("/rss/Sonars/Back", "rss_msgs/SonarMsg");   // back sonar
+		bumpSub = node.newSubscriber("/rss/BumpSensors", "rss_msgs/BumpMsg");         // bump sensors
+		odoSub = node.newSubscriber("/rss/odometry", "rss_msgs/OdometryMsg");         // odometry
+
+		// Describe message listeners
+		//
+		MessageListener<SonarMsg> sonarListener = new MessageListener<SonarMsg>() {
 				@Override
-				public void onNewMessage(SonarMsg message) {
-					//System.out.printf("Is Front?: %b\tRange: %.3f\n",message.isFront, message.range);
-					handleSonar(message);
+				public void onNewMessage(SonarMsg m) {
+					synchronized(this) {
+						sonars[m.isFront?g.FRONT:g.BACK] = m.range;
+					}
+					sp.newPoint(m.isFront,m.range);
 				}
-			});
-		sonarBackSub = node.newSubscriber("/rss/Sonars/Back", "rss_msgs/SonarMsg");
-		sonarBackSub.addMessageListener(new MessageListener<SonarMsg>() {
+			};
+
+		MessageListener<BumpMsg> bumpListener = new MessageListener<BumpMsg>() {
 				@Override
-				public void onNewMessage(SonarMsg message) {
-					//System.out.printf("Is Front?: %b\tRange: %.3f\n",message.isFront, message.range);
-					handleSonar(message);
+				public void onNewMessage(BumpMsg m) {
+					synchronized(this) {
+						bumpers[g.LEFT] = m.left;
+						bumpers[g.RIGHT] = m.right;
+					}
 				}
-			});
+			};
 
-		// initialize the ROS subscription to rss/BumpSensors
-		//
-		bumpSub = node.newSubscriber("/rss/BumpSensors", "rss_msgs/BumpMsg");
-		bumpSub.addMessageListener(new MessageListener<BumpMsg>() {
+		MessageListener<OdometryMsg> odoListener = new MessageListener<OdometryMsg>() {
 				@Override
-				public void onNewMessage(BumpMsg message) {
-					//System.out.printf("Left: %b\tRight: %b\n", message.left, message.right);
-					//handleBump(message);
+				public void onNewMessage(OdometryMsg m) {
+					double[] _pose = new double[3];
+					synchronized(this) {
+						pose[g.X] = m.x;
+						pose[g.Y] = m.y;
+						pose[g.THETA] = m.theta;
+						System.arraycopy(pose,0,_pose,0,pose.length);
+					}
+					sp.updateRobotPose(_pose);
 				}
-			});
+			}
 
-		// Initialize the ROS subscription to rss/odometry
-		// Don't need it right now
+		// Add message listeners to subscribers
 		//
-		odoSub = node.newSubscriber("/rss/odometry", "rss_msgs/OdometryMsg");
-		odoSub.addMessageListener(new MessageListener<OdometryMsg>() {
-				@Override
-				public void onNewMessage(OdometryMsg message) {
-					//System.out.printf("X: %.2f\tY: %.2f\ttheta: %.2f\n",message.x,message.y,message.theta);
-					handleOdometry(message);
-				}
-			});
+		sonarFrontSub.addMessageListener(sonarListener);
+		sonarBackSub.addMessageListener(sonarListener);
+		bumpSub.addMessageListener(bumpListener);
+		odoSub.addMessageListener(odoListener);
 
-		// Initialize the ROS publication to command/Motors
-		//
-		motorPub = node.newPublisher("/command/Motors","rss_msgs/MotionMsg");
-
-		// Initialize the ROS publication to graph points
-		pointPub = node.newPublisher("/gui/Point","lab5_msgs/GUIPointMsg");
-		// Initialize the ROS publication to graph lines
-		linePub = node.newPublisher("/gui/Line","lab5_msgs/GUILineMsg");
-		lsqWorld = new LeastSquareLine(linePub);
-		// // Initialize the ROS publication to graph line segments
-		// segmentPub = node.newPublisher("/gui/Segment","lab5_msgs/GUISegmentMsg");
-
-		// initialize the ROS publication to rss/state
-		//
-		statePub = node.newPublisher("/rss/state","std_msgs/String");
-		changeState(ALIGN_ON_BUMP);
-
-		Thread runningStuff = new Thread(this);
-		runningStuff.start();
+		new Thread(this).start();
 	}
 
 	public void onShutdown(Node node){
+		synchronized(this) {
+			shutdown = true;
+		}
+
 		if(node != null){
 			node.shutdown();
 		}
-	}
-
-	/* handle odometry
-	 * all position co-ordinates are stored as a matrix of 3*1 [x,y,theta]
-	 * all tranforms are done using matrix operations (functions from the Mat Class)
-	 */
-
-	public void handleOdometry(OdometryMsg message) {
-		robotToWorld = Mat.mul(Mat.translation(message.x, message.y), Mat.rotation(message.theta));
-	}
-
-	/**
-	 * Processes and prescribes response to a message from the bump sensor
-	 * @param message an OdometryMsg containing details about a bump sensor event
-	 */
-	public void handleBump(BumpMsg message) {
-		// Translational, and rotational velocities
-		//
-		double tv = 0;
-		double rv = 0;
-
-		if (state == STOP_ON_BUMP) {
-			// If we see a bump, then stop, otherwise we are controlled externally
-			//
-			if (message.right || message.left) {
-				tv = rv = 0;
-			} else {
-				return;
-			}
-		} else if (state == ALIGN_ON_BUMP) {
-			// Start aligning if a bump is pressed, otherwise we are controlled externally
-			//
-			if (message.right || message.left) {
-				changeState(ALIGNING);
-			}
-			else {
-				return;
-			}
-		} else if (state == ALIGNING) {
-			if (message.right && message.left) {
-				// if both sensors are depressed, then we are aligned
-				//
-				changeState(ALIGNED);
-				tv = rv = 0;
-			} else if (message.right || message.left) {
-				// If one bumper is depressed, then we need to rotate so that they are both depressed
-				// based on which bumper is hit, we need to choose the rotation direction
-				// We also have a small forward velocity to make sure the bumper that was depressed does not get undepressed
-				//
-				tv = ALIGNMENT_TRANSLATIONAL_SPEED*0.2;
-				int rotationalFactor = (message.left ? 1 : -1);
-				rv = rotationalFactor * ALIGNMENT_ROTATIONAL_SPEED;
-			} else {
-				// If neither is depressed, we move slowly forward.
-				//
-				rv = 0;
-				tv = ALIGNMENT_TRANSLATIONAL_SPEED;
-			}
-		} else if (state == ALIGNED) {
-			tv = rv = 0;
-		}
-		setMotorVelocities(tv,rv);
-	}
-
-	private boolean obstacleInRange(SonarMsg message) {
-		return message.range <= threshold_high && message.range >= threshold_low;
-	}
-
-	public void handleSonar(SonarMsg message) {
-		if (robotToWorld == null) {
-			return;
-		}
-		Mat sonarToRobot = message.isFront ? sonarFrontToRobot : sonarBackToRobot;
-
-		// get the range encoded as a pose vector
-		Mat echoSonar = Mat.encodePose(message.range, 0, 0);
-		// get the sonar position with respect to the world frame
-		Mat echoWorld = Mat.mul(robotToWorld, sonarToRobot, echoSonar);
-
-		double[] echoWorldL = Mat.decodePose(echoWorld);
-		Color pointColor = obstacleInRange(message) ? Color.RED : Color.BLUE;
-
-		if (obstacleInRange(message)) {
-			//lsqWorld.addPoint(echoWorldL[0], echoWorldL[1]);
-			//lsqWorld.publishLine();
-		}
-		int pointShape = message.isFront ? 0 : 1;
-		publishPoint((float) echoWorldL[0], (float) echoWorldL[1], pointColor, pointShape);
 	}
 
 	public void onShutdownComplete(Node node) {
@@ -249,44 +109,37 @@ public class LocalNavigation implements NodeMain,Runnable {
 		return new GraphName("/rss/localnavigation");
 	}
 
-	// Abstracts away sending the state message
-	//
-	private void changeState(java.lang.String newState){
-		state = newState;
-		org.ros.message.std_msgs.String msg = new org.ros.message.std_msgs.String();
-		msg.data = newState;
-		if(statePub != null) {
-			statePub.publish(msg);
-		}
-	}
+	/**
+	 * Runs the FSM
+	 * Calls step every 1/FSM.FREQ seconds
+	 */
+	public void run() {
+		long initial_time;
+		long duration;
+		long sleep_time;
 
-	// Useful for abstracting setting the motor velocities
-	//
-	private void setMotorVelocities(double tv, double rv) {
-		MotionMsg msg = new MotionMsg();
-		msg.translationalVelocity = tv;
-		msg.rotationalVelocity = rv;
-		if(motorPub != null) {
-			motorPub.publish(msg);
-		}
-	}
+		// For thread safe copying
+		//
+		boolean _shutdown;
+		double _sonars;
+		double _pose;
+		double _bumpers;
 
-	public static ColorMsg getColorMessage(Color color) {
-		ColorMsg message = new ColorMsg();
-		message.r = color.getRed();
-		message.b = color.getBlue();
-		message.g = color.getGreen();
-		return message;
-	}
+		do {
+			initial_time = System.currentTimeMillis();
 
-	private void publishPoint(float x, float y, Color color, int shape) {
-		GUIPointMsg pointPlot = new GUIPointMsg();
-		pointPlot.shape = shape;
-    	pointPlot.color = getColorMessage(color);
-		pointPlot.x = x;
-		pointPlot.y = y;
-		pointPub.publish(pointPlot);
+			// Update variables in a thread-safe manner
+			//
+			synchronized(this) {
+				_shutdown = shutdown;
+				System.arraycopy(sonars,  0, _sonars,  0, sonars.length );
+				System.arraycopy(pose,    0, _pose,    0, pose.length   );
+				System.arraycopy(bumpers, 0, _bumpers, 0, bumpers.length);
+			}
+			fsm.step(_sonars, _pose, _bumpers);
+			duration = System.currentTimeMillis()-initial_time;
+			sleep_time = ((long) (1000.0/FSM.FREQ)) - duration
+			Thread.sleep(sleep_time);
+		} while (_shutdown);
 	}
-
-	public void run() {}
 }
