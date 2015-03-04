9ad8f88e32abde6b2570c2a17cbd8ef5a8586cbb
Eric Huppert <eric@erichuppert.com>
2015-03-04 12:36:56 -0500

    fixed haveObstacle

--- lab5/src/LocalNavigation/FSM.java
+++ lab5/src/LocalNavigation/FSM.java
@@ -289,7 +289,7 @@ public class FSM {
 	// determines if EITHER sensor is encountering an obstacle, based on the sonar threshold
 	//
 	private boolean haveObstacle() {
-		return SonarPoints.obstacleInRange(sonars[g.BACK]) || SonarPoints.obstacleInRange(sonars[g.BACK]);
+		return SonarPoints.obstacleInRange(sonars[g.BACK]) || SonarPoints.obstacleInRange(sonars[g.FRONT]);
 	}
 	
 	
