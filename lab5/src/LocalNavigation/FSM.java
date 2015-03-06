4474c654a35ebc4e8262a0602bed511c17cee9be
Rodrigo Toste Gomes <rgomes@mit.edu>
2015-03-05 22:35:38 -0500

    Derp

--- lab5/src/LocalNavigation/FSM.java
+++ lab5/src/LocalNavigation/FSM.java
@@ -340,7 +340,7 @@ public class FSM {
 	private void wall_ended() {
 		setVelocities = true;
 		tv = ALIGNMENT_TRANSLATIONAL_SPEED;
-		rv = ALIGNMENT_ROTATIONAL_SPEED/OBSTACLE_RETREAT_DISTANCE * 1./5.;
+		rv = ALIGNMENT_TRANSLATIONAL_SPEED/OBSTACLE_RETREAT_DISTANCE * 1./5.;
 		//changeState(ALIGN_ON_BUMP);
 	}
 
