e4b55e8f864057cd340ef8bd3d4d542ec1dc4336
Rodrigo Toste Gomes <rgomes@mit.edu>
2015-03-05 22:34:48 -0500

    Fudging rotation

--- lab5/src/LocalNavigation/FSM.java
+++ lab5/src/LocalNavigation/FSM.java
@@ -341,7 +341,7 @@ public class FSM {
 	private void wall_ended() {
 		setVelocities = true;
 		tv = ALIGNMENT_TRANSLATIONAL_SPEED;
-		rv = 0.1;
+		rv = ALIGNMENT_ROTATIONAL_SPEED/OBSTACLE_RETREAT_DISTANCE * 1./5.;
 		//changeState(ALIGN_ON_BUMP);
 	}
 
