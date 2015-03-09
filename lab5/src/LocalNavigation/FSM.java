b4fb848c51f616fc83c18b98aafdbcfbea42d3e2
Rodrigo Toste Gomes <rgomes@mit.edu>
2015-03-09 15:08:10 -0400

    Forward code for testing motor

--- lab5/src/LocalNavigation/FSM.java
+++ lab5/src/LocalNavigation/FSM.java
@@ -30,6 +30,7 @@ public class FSM {
 	public static final int WALL_ENDED          = 10;
 	public static final int START_STATE         = 11;
 	public static final int DONE                = 12;
+	public static final int FORWARD             = 13;
 
 	// State descriptions
 	//
@@ -46,7 +47,8 @@ public class FSM {
 		"Tracking the wall with sonars",
 		"Found the end of the wall",
 		"Initialization State",
-		"Found the final state, finished."
+		"Found the final state, finished.",
+		"Moving Forward"
 	};
 
 	// State variable
@@ -59,7 +61,7 @@ public class FSM {
 	private double[] sonars;
 	private double[] pose;
 	private boolean[] bumpers;
-	
+
 
 	// SonarPoints to control the linear filter/segments
 	//
@@ -166,6 +168,11 @@ public class FSM {
 		case WALL_ENDED:
 			wall_ended();
 			break;
+		case DONE:
+			done();
+			break;
+		case FORWARD:
+			forward();
 		default:
 			throw new RuntimeException();
 		}
@@ -366,6 +373,14 @@ public class FSM {
 	// Goes into this state when we have found a model of the obstacle
 	//
 	private void done() {}
+
+	// Moves forward: for testing motor asymmetry
+	//
+	private void forward() {
+		setVelocities = true;
+		tv = ALIGNMENT_TRANSLATIONAL_SPEED;
+		rv = 0;
+	}
 
 	// determines if EITHER sensor is encountering an obstacle, based on the sonar threshold
 	//
