336ef72b651b9cc59d4dc96022bc2797ed26465a
Eric Huppert <eric@erichuppert.com>
2015-03-09 18:23:22 -0400

    turn off displayMap()

--- lab6/src/GlobalNavigation/GlobalNavigation.java
+++ lab6/src/GlobalNavigation/GlobalNavigation.java
@@ -44,7 +44,7 @@ public class GlobalNavigation implements NodeMain{
 		try {
 			System.out.printf("==============\n\nThe map file name is %s\n\n", mapFileName);
 			polygonMap = new PolygonMap(mapFileName);			
-			displayMap();
+//			displayMap();
 		} catch (Exception e) {
 			System.out.println(e);
 			throw new RuntimeException(e.getMessage());
