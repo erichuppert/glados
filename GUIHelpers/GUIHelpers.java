eb3bbb7f8d185a87bfa4252241c5908f6fa65192
JaguarK <jaguark@mit.edu>
2015-03-09 16:06:36 -0400

    added skeleton of GUIHelpers

---
+++ GUIHelpers/GUIHelpers.java
@@ -0,0 +1,16 @@
+package GUIHelpers;
+
+public class GUIHelpers {
+
+	public static Color getRandomColor() {
+		
+	}
+	
+	public static ColorMsg getColorMsg(Color color) {
+		
+	}
+	
+	public static ColorMsg getRandomColorMsg() {
+		
+	}
+}
