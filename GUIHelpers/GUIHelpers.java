76ffba80cf48f8c1e5cbf2a6e7711bc7904e9def
JaguarK <jaguark@mit.edu>
2015-03-09 16:18:32 -0400

    finished GUIHelpers color methods

--- GUIHelpers/GUIHelpers.java
+++ GUIHelpers/GUIHelpers.java
@@ -1,16 +1,28 @@
 package GUIHelpers;
 
-public class GUIHelpers {
+import java.awt.Color;
 
-	public static Color getRandomColor() {
-		
+import src.LocalNavigation.ColorMsg;
+
+public class GUIHelpers {
+	
+	public static Color randomColor() {
+		float r = rand.nextFloat();
+		float g = rand.nextFloat();
+		float b = rand.nextFloat();
+		Color rndC = new Color(r,g,b);
+		return rndC.darker();
 	}
 	
-	public static ColorMsg getColorMsg(Color color) {
-		
+	public static ColorMsg colorMessage(Color color) {
+		ColorMsg msg = new ColorMsg();
+		msg.r = color.getRed();
+		msg.b = color.getBlue();
+		msg.g = color.getGreen();
+		return msg;
 	}
 	
-	public static ColorMsg getRandomColorMsg() {
-		
+	public static ColorMsg randomColorMsg() {
+		return colorMessage(randomColor()); 
 	}
 }
