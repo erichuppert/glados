afb2c8ffb72af431b010dece6bae15c98963f8bb
Rodrigo Toste Gomes <rgomes@mit.edu>
2015-03-11 02:07:27 -0400

    Fixed WaypointNavigator bugs

--- lab6/src/GlobalNavigation/WaypointNavigator.java
+++ lab6/src/GlobalNavigation/WaypointNavigator.java
@@ -11,49 +11,45 @@ import org.ros.message.lab5_msgs.*;
 import org.ros.message.rss_msgs.*;
 
 public class WaypointNavigator {
-	
 	private List<WaypointNode> wayPoints;
 	//ROS Publishers
 	//
 	private Publisher<MotionMsg> motorPub; // motor commands
-	
+
 	private double[] robotPose;
 	private int nextPointInd = 1;
 	private Line2D.Double currentTrajectory;
 	private Node node;
-	
-	
+
 	public WaypointNavigator(Node _node, List<WaypointNode> _wayPoints) {
 		wayPoints = _wayPoints;
 		node = _node;
 		motorPub = node.newPublisher("/command/Motors","rss_msgs/MotionMsg");
 		currentTrajectory = new Line2D.Double(wayPoints.get(nextPointInd-1).getValue(), wayPoints.get(nextPointInd).getValue());
 	}
-	
-		
 
 	//Translational/Rotational velocities
 	// So we don't have to redeclare them in every method.
 	// Set by methods to change motor velocities.
 	//
 	private double tv,rv;
-	
-	
+
 	// Below are values that have been tuned based on experimentation
 	//
 	private static float TRANSLATIONAL_SPEED = (float) 0.05;
 	private static float ROTATIONAL_SPEED = (float) 0.1;
-	
+
 	public boolean step(double[] _robotPose) {
 		robotPose = _robotPose;
-		Point2D.Double robotPoint = new Point2D.Double(robotPose[g.X], robotPose[g.Y]); 
+		Point2D.Double robotPoint = new Point2D.Double(robotPose[g.X], robotPose[g.Y]);
 		WaypointNode nextNode = wayPoints.get(nextPointInd);
-		
+
 		if ( nearPoint(robotPoint, nextNode.getValue()) ) {
-			nextPointInd++;			
+			nextPointInd++;
 			if ( nextPointInd == wayPoints.size() ) {
 				return true;
-			} 
+			}
+			currentTrajectory = new Line2D.Double(wayPoints.get(nextPointInd-1).getValue(), wayPoints.get(nextPointInd).getValue());
 		}
 		double angleError = getAngleError();
 		// check if we still need to rotate towards the next point
@@ -69,30 +65,24 @@ public class WaypointNavigator {
 			double Kd = 2.5;
 			double Ka = 0.5;
 			double distanceError = getDistanceError();
-			if (distanceError > 0.001 || angleError > 0.001) {
-				double theta_i = -Kd*distanceError;
-				rv = -Ka*(theta_i - angleError);
-			} else {
-				rv = 0;
-			}				
+			double theta_i = -Kd*distanceError;
+			rv = -Ka*(theta_i - angleError);
 		}
 		setMotorVelocities(tv, rv);
 		return false;
 	}
 
-	
 	// 10cm threshold
 	//
 	public static double POINT_THRESHOLD_DIST = 0.1; 
-	
+
 	// Checks if current robot position is close enough to its destination wayPoint 
 	// to continue towards the next wayPoint
-	//	
+	//
 	private boolean nearPoint(Point2D.Double p1, Point2D.Double p2) {
 		return p1.distance(p2) <= POINT_THRESHOLD_DIST;
-	}	
-	
-	
+	}
+
 	public synchronized double getDistanceError() {
 		if(robotPose == null) {
 			return 0;
@@ -121,7 +111,7 @@ public class WaypointNavigator {
 			return diff;
 		}
 	}
-	
+
 	// Useful for abstracting setting the motor velocities
 	//
 	private void setMotorVelocities(double tv, double rv) {
