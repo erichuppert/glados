4d5ad27f706b06c0ece9947f4621e971da5866a2
Rodrigo Toste Gomes <rgomes@mit.edu>
2015-03-10 20:14:46 -0400

    No filling

--- lab6/src/GlobalNavigation/GlobalNavigation.java
+++ lab6/src/GlobalNavigation/GlobalNavigation.java
@@ -153,7 +153,7 @@ public class GlobalNavigation implements NodeMain{
 	/**
 	 * Determines if the GUI should display obstacles as filled
 	 */
-	private static final boolean FILL_GUI_OBSTACLES = true;
+	private static final boolean FILL_GUI_OBSTACLES = false;
 
 	private void drawPolygon(PolygonObstacle obstacle) {
 
