e85695a87c2ead02093fef8d48d5e8ca6ca140b7
Eric Huppert <eric@erichuppert.com>
2015-03-11 12:13:13 -0400

    typo

--- lab6/src/GlobalNavigation/AStar.java
+++ lab6/src/GlobalNavigation/AStar.java
@@ -45,7 +45,7 @@ public class AStar<V> {
 			double cost = entry.cost;
 			System.err.printf("Current node cost is %f\n", cost);
 			GraphNode<V> current = entry.node;
-			System.err.printf("This node is at %s\nGoal node at %s\n", current.getValue().toString(), goal.);
+			System.err.printf("This node is at %s\nGoal node at %s\n", current.getValue().toString(), goal.toString());
 			if (reachedEnd.test(current.getValue())) {
 				System.err.println("Found the end!");
 				end = current;
