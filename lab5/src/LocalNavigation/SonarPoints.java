8f513513e8846497f70b93fd856e8a6aa03b6747
Rodrigo Toste Gomes <rgomes@mit.edu>
2015-03-11 18:42:12 -0400

    Fixing threshold

--- lab5/src/LocalNavigation/SonarPoints.java
+++ lab5/src/LocalNavigation/SonarPoints.java
@@ -52,7 +52,7 @@ public class SonarPoints {
 	// Threshold for finding obstacles.
 	// Values decided on based on parameters of the problem.
 	//
-	private static final double threshold_high = 1.0;
+	private static final double threshold_high = 0.6;
 	private static final double threshold_low = 0.05;
 	private static final double threshold_highest = 2.0;
 
