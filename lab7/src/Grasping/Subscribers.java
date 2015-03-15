39322300542ebcabf5077e2030940a9f7cf1bcbb
Rodrigo Toste Gomes <rgomes@mit.edu>
2015-03-15 17:24:47 -0400

    Bug fixing

--- lab7/src/Grasping/Subscribers.java
+++ lab7/src/Grasping/Subscribers.java
@@ -28,11 +28,11 @@ public class Subscribers {
 
 		public void onNewMessage(T m) {
 			switch(type) {
-			case SONAR: g.setSonars((SonarMsg)m); break;
-			case BUMP: g.setBumps((BumpMsg)m); break;
-			case ODO: g.setPose((OdometryMsg)m); break;
-			case CAMERA: g.setCamera((org.ros.message.sensor_msgs.Image)m); break;
-			case ARM: g.setArm((ArmMsg)m);
+			case ListenerType.SONAR: g.setSonars((SonarMsg)m); break;
+			case ListenerType.BUMP: g.setBumps((BumpMsg)m); break;
+			case ListenerType.ODO: g.setPose((OdometryMsg)m); break;
+			case ListenerType.CAMERA: g.setCamera((org.ros.message.sensor_msgs.Image)m); break;
+			case ListenerType.ARM: g.setArm((ArmMsg)m);
 			default: g.assertTrue("Invalid Listener Type", false);
 			}
 		}
