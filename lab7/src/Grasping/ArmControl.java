0561853bba870463bf0747246ee1c710e1b810bd
Rodrigo Toste Gomes <rgomes@mit.edu>
2015-03-15 23:16:13 -0400

    Bug Fixing

--- lab7/src/Grasping/ArmControl.java
+++ lab7/src/Grasping/ArmControl.java
@@ -17,6 +17,8 @@ public class ArmControl {
 	private static double shoulder = 0.244; // Linkage distance [m] between Shoulder Pivot and Wrist Pivot
 	private static double shoulderHeight = 0.26; // Height [m] of the shoulder pivot
 	private static double wrist = 0.065; // Linkage distance [m] between Wrist Pivot and Gripper Pivot
+	private static final double min_height = shoulderHeight-shoulder;
+	private static final double max_height = shoulderHeight+shoulder-0.05; // Wrist limits cause the -0.05
 
 	public static double getThetaShoulder(){
 		return thetaShoulder;
