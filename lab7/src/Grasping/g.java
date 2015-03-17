40a3a3f4aac8af3938add319805ae513e765fd10
Eric Huppert <eric@erichuppert.com>
2015-03-17 16:04:04 -0400

    updated parameter

--- lab7/src/Grasping/g.java
+++ lab7/src/Grasping/g.java
@@ -27,8 +27,8 @@ public class g {
 	 * Gripper states
 	 */
 	public static final double CLOSED = 0.95;
-	public static final double OPEN   = CLOSED;
-	public static final double MIDDLE = 0.5;
+	public static final double OPEN   = 0.05;
+	public static final double MIDDLE = CLOSED;
 
 	/**
 	 * Publishers to send commands to the robot.
