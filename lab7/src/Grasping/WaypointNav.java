8f5ebfa20e73e00112e8d4921ca8efaf6ad16eca
Rodrigo Toste Gomes <rgomes@mit.edu>
2015-03-18 17:54:44 -0400

    Bug fixing

--- lab7/src/Grasping/WaypointNav.java
+++ lab7/src/Grasping/WaypointNav.java
@@ -56,50 +56,48 @@ public class WaypointNav implements Runnable {
 			}
 			done = false;
 
-			synchronized (this) {
-				while (!stop) {
-					double tv = 0;
-					double rv = 0;
-					double[] robot = g.getPose();
-					double angleToWP = getAngleError(robot,wp);
-					double dAngle = wp[g.THETA] - robot[g.THETA];
-					dAngle = atan2(sin(dAngle), cos(dAngle));
-					double distance = getDistanceError(robot,wp);
+			while (!stop) {
+				double tv = 0;
+				double rv = 0;
+				double[] robot = g.getPose();
+				double angleToWP = getAngleError(robot,wp);
+				double dAngle = wp[g.THETA] - robot[g.THETA];
+				dAngle = atan2(sin(dAngle), cos(dAngle));
+				double distance = getDistanceError(robot,wp);
 
-					if (distance > distanceThreshold) {
-						// We are not at the right location
-						//
-						int sign = g.sign(cos(angleToWP));
-						tv = Kd*distance*abs(pow(cos(angleToWP), straightness))*sign;
-						if (sign == -1) {
-							angleToWP = angleToWP-PI;
-							angleToWP = atan2(sin(angleToWP),cos(angleToWP));
-						}
-						rv = Ka*angleToWP;
-					} else if (abs(dAngle) > angleThreshold) {
-						// We're at the right location, but don't have the right angle.
-						//
-						tv = 0;
-						rv = Ka*dAngle;
-					} else {
-						// We've reached our final pose.
-						//
-						break;
+				if (distance > distanceThreshold) {
+					// We are not at the right location
+					//
+					int sign = g.sign(cos(angleToWP));
+					tv = Kd*distance*abs(pow(cos(angleToWP), straightness))*sign;
+					if (sign == -1) {
+						angleToWP = angleToWP-PI;
+						angleToWP = atan2(sin(angleToWP),cos(angleToWP));
 					}
+					rv = Ka*angleToWP;
+				} else if (abs(dAngle) > angleThreshold) {
+					// We're at the right location, but don't have the right angle.
+					//
+					tv = 0;
+					rv = Ka*dAngle;
+				} else {
+					// We've reached our final pose.
+					//
+					break;
+				}
 
-					g.pubs.setMotorVelocities(tv,rv);
-					try {
-						Thread.sleep(1000/updateFreq);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-						g.pubs.setMotorVelocities(0,0);
-						return;
-					}
+				g.pubs.setMotorVelocities(tv,rv);
+				try {
+					Thread.sleep(1000/updateFreq);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					g.pubs.setMotorVelocities(0,0);
+					return;
 				}
 			}
-			g.pubs.setMotorVelocities(0,0);
-			stop = false;
 		}
+		g.pubs.setMotorVelocities(0,0);
+		stop = false;
 	}
 
 	private double getAngleError(double[] from, double[] to) {
