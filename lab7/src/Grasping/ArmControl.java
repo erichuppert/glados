b3e7b994caa5b7a95fe4015be832622cd5a7e5f0
Rodrigo Toste Gomes <rgomes@mit.edu>
2015-03-18 13:45:30 -0400

    Bug fixing?

--- lab7/src/Grasping/ArmControl.java
+++ lab7/src/Grasping/ArmControl.java
@@ -27,7 +27,7 @@ public class ArmControl implements Runnable {
 	private static final double wristLength = 0.065;
 
 	// Minimum, and maximum Z positions that we can set
-	private static final double minHeight = 0.045;//shoulderHeight-shoulderLength;
+	private static final double minHeight = 0.035;//shoulderHeight-shoulderLength;
 	private static final double maxHeight = shoulderHeight+shoulderLength-0.005;
 
 	// Gripper control
