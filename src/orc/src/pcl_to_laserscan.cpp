fa5dac7e8e5e658611e6dfffe9316ade70e8c94e
Rodrigo Toste Gomes <rgomes@mit.edu>
2015-04-22 02:17:07 -0400

    No longer publishing cloud from base_link frame (we've learned how to convert things), and adding parameters to pcl_to_laserscan

--- src/orc/src/pcl_to_laserscan.cpp
+++ src/orc/src/pcl_to_laserscan.cpp
@@ -12,14 +12,20 @@
 #include "pcl_ros/point_cloud.h"
 #include "pcl_ros/transforms.h"
 
+#include <string>
 #include <cmath>
 #include <cstring>
 
-#define BASE_LINK "base_link"
+#define DEFAULT_PCL_TOPIC "camera/depth/points"
+#define DEFAULT_SCAN_TOPIC "pcl_scan"
+#define DEFAULT_BASE_LINK "base_link"
+
+std::string pcl_topic;
+std::string scan_topic;
+std::string base_link;
 
 tf::TransformListener *tf_listener;
 ros::Publisher scan_pub;
-ros::Publisher point_pub;
 
 void cloud_cb(const sensor_msgs::PointCloud2::ConstPtr& input) {
     ros::NodeHandle nh;
@@ -27,18 +33,17 @@ void cloud_cb(const sensor_msgs::PointCloud2::ConstPtr& input) {
     pcl_conversions::toPCL(*input, pcl_pc);
     pcl::PointCloud<pcl::PointXYZ> cloud;
 
-    //pcl_pc.header.frame_id = "camera_link";
     pcl_pc.header.stamp = 0;
     pcl::fromPCLPointCloud2(pcl_pc,cloud);
 
-    tf_listener->waitForTransform(BASE_LINK, pcl_pc.header.frame_id, ros::Time(pcl_pc.header.stamp), ros::Duration(5.0));
+    tf_listener->waitForTransform(base_link, pcl_pc.header.frame_id, ros::Time(pcl_pc.header.stamp), ros::Duration(5.0));
 
     pcl::PointCloud<pcl::PointXYZ> cloud_out;
-    pcl_ros::transformPointCloud(BASE_LINK, cloud, cloud_out, *tf_listener);
+    pcl_ros::transformPointCloud(base_link, cloud, cloud_out, *tf_listener);
     sensor_msgs::LaserScan output;
 
     output.header.stamp = (*input).header.stamp;
-    output.header.frame_id = BASE_LINK;
+    output.header.frame_id = base_link;
 
     unsigned int num_readings = 300;
     double laser_frequency = 10000;
@@ -54,7 +59,6 @@ void cloud_cb(const sensor_msgs::PointCloud2::ConstPtr& input) {
     double z_max = 0.1;
     int range_num[num_readings];
     memset(range_num, 0x0, sizeof(range_num));
-    point_pub.publish(cloud_out);
     pcl::PointCloud<pcl::PointXYZ>::iterator it;
     for (it = cloud_out.begin(); it != cloud_out.end(); it++) {
         if (it->z > z_min && it->z < z_max) {
@@ -84,12 +88,17 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "pointcloud2_to_laserscan");
     ros::NodeHandle nh;
 
+    // Get parameters
+    //
+    nh.param<std::string>("pcl_topic", pcl_topic, DEFAULT_PCL_TOPIC);
+    nh.param<std::string>("scan_topic", scan_topic, DEFAULT_SCAN_TOPIC);
+    nh.param<std::string>("base_link", base_link, DEFAULT_BASE_LINK);
+
     // Create ROS subscriber for input point cloud
     //
-    ros::Subscriber sub = nh.subscribe("camera/depth/points", 1, cloud_cb);
-    scan_pub = nh.advertise<sensor_msgs::LaserScan>("pcl_scan", 1);
+    ros::Subscriber sub = nh.subscribe(pcl_topic, 1, cloud_cb);
+    scan_pub = nh.advertise<sensor_msgs::LaserScan>(scan_topic, 1);
     tf_listener = new tf::TransformListener();
-    point_pub = nh.advertise<pcl::PointCloud<pcl::PointXYZ>>("cloud_out", 50);
 
     // Spin
     //
