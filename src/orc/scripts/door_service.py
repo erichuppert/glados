be431ff13ed90a027f325bcd385bbd9b3ad46222
GLaDOS <rssworkstation@gmail.com>
2015-05-03 17:37:05 -0400

    Door Service

--- src/orc/scripts/door_service.py
+++ src/orc/scripts/door_service.py
@@ -1,35 +1,42 @@
+#!/usr/bin/python2
 import rospy, math
 from orc.srv import OpenCloseDoor
+from orc.msg import JointSet
 from sensor_msgs.msg import JointState
 
 OPEN_ANGLE = math.pi/2.0
 CLOSED_ANGLE = 0.0
 
 EPSILON = 0.1
-
-door_angle = 0.0
+DOOR_NAME = "door"
+door_angle = -0.1
 
 def open_close_door(req):
     if req.state == 1:
         desired_angle = OPEN_ANGLE
     else:
         desired_angle = CLOSED_ANGLE
+
+    loop = rospy.rate(30)
     while abs(door_angle - desired_angle) > EPSILON:
-        pass
+        joint = JointSet()
+        joint.joint_name=DOOR_NAME
+        joint.angle=desired_angle
+        joint_state_pub.publish(joint)
+        loop.sleep()
     return
-            
 
 def handle_joint_msg(msg):
-    door_servo_index = msg.name.index("door")
+    door_servo_index = msg.name.index(DOOR_NAME)
     door_angle = msg.position[door_servo_index]
 
 def main():
+    global joint_state_pub
     rospy.init_node('door_open_close_server')
-    s = rospy.Service('open_close_door', AddTwoInts, open_close_door)
-    joint_state_pub = rospy.Publisher("joint_set", JointStateMsg, queue_size=10)
-    joint_angle_sub = rospy.Subscriber("joint_state", JointMsg, hande_joint_msg)
+    s = rospy.Service('open_close_door', OpenCloseDoor, open_close_door)
+    joint_state_pub = rospy.Publisher("joint_set", JointSet, queue_size=10)
+    rospy.Subscriber("joint_state", JointState, handle_joint_msg)
     rospy.spin()
 
 if __name__ == '__main__':
     main()
-        
