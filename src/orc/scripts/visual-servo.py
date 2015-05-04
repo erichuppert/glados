6d56447d2ea314f9b5e054d7d3eae808a847c49b
Eric Huppert <eric@erichuppert.com>
2015-05-03 20:02:18 -0400

    created visual servo service

--- src/orc/scripts/visual-servo.py
+++ src/orc/scripts/visual-servo.py
@@ -3,6 +3,7 @@ import rospy
 import math
 from geometry_msgs.msg import Twist, Point
 from orc.msg import TouchState
+from orc.srv import VisualServoResponse, VisualServo
 
 TARGET_BLOCK_SIZE = 1100
 ALIGNED_WIDTH = 250
@@ -14,27 +15,45 @@ FORWARD_GAIN = .5
 DESIRED_DIST = 0.15 # Experimental
 STRAIGHTNESS = 100
 
+paused = true
+done = false
+
 def handle_block_msg(block_msg):
-    angle_error = (block_msg.y - DESIRED_ROTATION)
-    distance_error = (block_msg.x - DESIRED_DIST)
-    print "error is " + str(angle_error)
-    rotational_vel = angle_error*ROTATION_GAIN
-    msg = Twist()
-    msg.linear.x = math.cos(angle_error)**STRAIGHTNESS * FORWARD_GAIN * distance_error
-    msg.angular.z = rotational_vel
-    print msg
-    vel_pub.publish(msg)
+    if not paused:
+        angle_error = (block_msg.y - DESIRED_ROTATION)
+        distance_error = (block_msg.x - DESIRED_DIST)
+        print "error is " + str(angle_error)
+        rotational_vel = angle_error*ROTATION_GAIN
+        msg = Twist()
+        msg.linear.x = math.cos(angle_error)**STRAIGHTNESS * FORWARD_GAIN * distance_error
+        msg.angular.z = rotational_vel
+        print msg
+        vel_pub.publish(msg)
 
 def handle_bump_msg(bump_msg):
+    global done
     if (bump_msg.gripper):
         vel_pub.publish(Twist())
+    # call the pickup and drop service here
+    if not paused:
+        done = true
 
+def visual_servo_service(req):
+    global paused, done
+    paused = false
+    done = false
+    loop = rospy.rate(30)
+    while not done:
+        loop.sleep()
+    return VisualServoResponse()
+        
 def main():
     global vel_pub
     rospy.init_node("visual_servo")
     nearest_block_sub = rospy.Subscriber("nearest_block", Point, handle_block_msg)
     bump_sub = rospy.Subscriber("touch", TouchState, handle_bump_msg)
     vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=5)
+    s = rospy.Service('VisualServo', VisualServo, visual_servo_service)
     rospy.spin()
 
 if __name__ == "__main__":
